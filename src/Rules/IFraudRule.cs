b0a1705d2c5a831e81d70c9b10171f7c4fb31b19
akhyhari <akhy.kays@gmail.com>
2026-04-10 23:55:29 +0200

    feat: interface IFraudRule for future implémentations (HighAmountRule, SuspiciousIpRule..)

---
+++ src/Rules/IFraudRule.cs
@@ -0,0 +1,13 @@
+﻿using TrainingNetCourses.FraudCheck.Models;
+
+namespace TrainingNetCourses.FraudCheck.Rules;
+
+public interface IFraudRule
+{
+    string RuleId { get; }
+    string Description { get; }
+    int Weight { get; }
+
+    bool Evaluate(Order order);
+}
+
